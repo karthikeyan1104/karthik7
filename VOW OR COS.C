3ccdd91c59ee72a4b8f1b5da22d2bd285d2d5ead
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-01-21 16:22:02 +0530

    Create VOW OR COS.C
    TO FIND A CHAR IS VOWEL OR CONSONENT

---
+++ VOW OR COS.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    int A, B;
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+    A = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    A = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    if (A ||B)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
+}
