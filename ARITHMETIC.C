6cebfcdacfa01dc4842912ddef6bc0eb30450e95
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-02 16:15:38 +0530

    ARITHMETIC.C
    NIL

--- ARITHMETIC.C
+++ ARITHMETIC.C
@@ -4,6 +4,7 @@ int main()
 float a,s,d,n;
 scanf("%f%f%f",&a,&d,&n);
 printf("\n the initial value is %.2f ,the difference is %.2f ,the total number of value is %.2f ",a,d,n);
+printf("\n get the value");
 s=(n*(2*a+(n-1)*d))/2;
 printf("\n the sum of arithemtic progression is %.2f ",s);
 return 0;
