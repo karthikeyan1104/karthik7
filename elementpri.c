021a5a96fc36ef1183c99e075eb51982695929db
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-02 16:46:11 +0530

    elementpri.c
    nil

---
+++ elementpri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n;
+printf("Enter the number");
+printf("\n enter the num");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",a[i],i);
+}
+}
