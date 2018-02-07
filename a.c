c6a3943f4338d7a59142a6ef77e921580b0bdb25
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-07 15:55:57 +0530

    a.c

---
+++ a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+printf("\n enter the value");
+char a[50]="hellow word";
+int c=0,i;
+for(i=0;a[i]=0;i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("%d",c);
+return 0;
+}
