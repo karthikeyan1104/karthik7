7241f96fe28b07b92213a9b15559adb017e2ecb4
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-07 16:37:11 +0530

    k.c

---
+++ k.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,a;
+  printf("\n");
+	scanf("%d",&n);
+	scanf("%d",&a);
+	if(a<=n)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf(" no");
+	}
+	return 0;
+}
