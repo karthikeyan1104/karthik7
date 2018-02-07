a401c0e68b2cc2fcd55520eb6f6fe4789695797c
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-07 16:34:51 +0530

    i.c

---
+++ i.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s1[50],char s2[50];
+clrscr();
+printf("enter string1");
+printf("\n");
+scanf("%s",s1);
+printf("enter string2");
+scanf("%s",s2);
+printf("%s%s",s1,s2);
+getch();
+}
