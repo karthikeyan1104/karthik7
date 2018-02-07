e2f844d8b9f119df12c0254c2a8631466502fec1
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-07 16:16:16 +0530

    e.c

---
+++ e.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int i = 65;
+    printf(" value of i=%d before swapping", i, k);
+    printf("\n");
+    i = i ^ k;
+    printf("value of i=%d after swapping", i, k);
+    return 0;
+}
