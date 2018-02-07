909fe87ebfd8e88b2e1b49072176e5d8d56a06ca
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-07 16:39:11 +0530

    l.c

---
+++ l.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a=0,i;
+printf("enter the number: \n");
+printf("\n);
+scanf("%d",&a);
+while(a!=0)
+{
+    a=a/10;
+    ++i;
+}
+    printf("count number is :%d",i);
+}
+
+© 2018 GitHub, Inc.
