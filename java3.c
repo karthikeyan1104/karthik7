20160354f34f709b97754818fde8fc5060d1b954
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-12 16:42:25 +0530

    java3.c

---
+++ java3.c
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sum
+{
+public static void main(String aa[])
+{
+int z,b,f;
+Scanner s=new Scanner(System.in);
+z=s.nextInt();
+b=s.nextInt();
+f=z+b;
+System.out.println(f);
+}
+}
