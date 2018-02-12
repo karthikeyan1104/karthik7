74b1d2d763c514785665c14426a91bb800be406d
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-12 16:44:43 +0530

    java4.c

---
+++ java4.c
@@ -0,0 +1,23 @@
+es (22 sloc)  412 Bytes
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class poww{
+     public static void main(String aa[]){
+     int z,b,f=0;
+     Scanner s=new Scanner(System.in);
+     z=s.nextInt();
+     while(a!=1){
+         if(z%2!=0){
+             f=1;
+         }
+         z=z/2;
+        }
+     if(f==1){
+         System.out.println("No");
+     }
+ else{
+         System.out.println("Yes");
+ }
+    }
+}
