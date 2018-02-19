1c577e867ef177c048c84d3bbe144e29d9a7e404
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-19 16:22:55 +0530

    name10.c

---
+++ name10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+printf("d");
+    char s[20];
+    int n,i;
+    scanf("%s",s);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",s[i]);
+    }
+    return 0;
+}
