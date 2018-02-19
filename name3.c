02cb93ff692b7fd424ec25a1ad8e68c9de6981f0
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-19 15:50:18 +0530

    name3.c

---
+++ name3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int n;
+ 
+   printf("Enter an integer\n");
+   printf("enter the value");
+   scanf("%d", &n);
+    if(n%2!=0)
+    n=n-1;
+    else
+    n=n-2;
+    printf("%d",n);
+    return 0;
+}
