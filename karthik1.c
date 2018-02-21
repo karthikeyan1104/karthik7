58f7da2bba0eefce3e27be0a62e295b0de15fbf2
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-21 15:39:00 +0530

    karthik1.c

---
+++ karthik1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+printf("N");
+	char a[100];
+	int n,i,num;
+	scanf("%s",a);
+	scanf("%d",&num);
+	n=strlen(a);
+    for(i=0;i<num;i++)
+    {
+        printf("%c",a[i]);
+    }
+	return 0;
+}
