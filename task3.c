9c34eec4cd2a166a65d072ced450d490130725c2
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-28 15:38:50 +0530

    Create task3.c

---
+++ task3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{
+	float n;
+	int a;
+	scanf("%f",&n);
+	if(n<0)
+	{
+  printf("enter the value");
+	    a=(int)(n-0.5);
+	}
+	else
+	{
+	    a=(int)(n+0.5);
+	}
+	printf("%d",a);
+	return 0;
+}
