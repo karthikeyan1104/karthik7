23f1881b75a35f71fa6476e75c71dfd50c68aa98
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-02-28 15:46:40 +0530

    Create task7.c

---
+++ task7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+    printf("JIND");
+        printf("yes...%d is a multiple of 13",n);
+    }
+    else
+    {
+    printf("no");
+    }
+	return 0;
+}
