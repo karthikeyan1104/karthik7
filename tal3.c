ddc422b884c50244523eaddde83fcbdb8c54c020
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-03-05 15:53:45 +0530

    Create tal3.c

---
+++ tal3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int l,b,h,tsa,volume;
+  printf("F");
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("volume=%d\n",volume);
+	printf("TSA=%d",tsa);
+	return 0;
+}
