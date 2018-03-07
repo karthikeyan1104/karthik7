dcac25b24cd7432d6adcf1701b8ee4649e74a9e5
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-03-07 15:42:28 +0530

    Create part2.c

---
+++ part2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int n,a[n],i,sum=0;
+  printf("enter the value");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a);
+	    sum=sum+a;
+	}
+	printf("%d",sum);
+	return 0;
+}
