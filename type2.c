3f7e0327e48cd0b6e6959a883186e89ebfa1d429
karthikjothi <35525757+karthikjothi@users.noreply.github.com>
2018-03-16 15:49:13 +0530

    Create type2.c

---
+++ type2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int sum=0,i,n;
+    printf("enter the value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	    sum=sum+i;
+	}
+	printf("%d",sum);
+	return 0;
+}
